eb8325702f839097045940cd1eb8d3959dd86840
rautgopa2010-design <rautgopa2010@gmail.com>
2026-05-01 15:22:24 +0000

    variales done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=789898
+let accountEmail="rautgopa2010@gmail.com"
+var accountPassword="1234567"
+// accountCity= "Bengaluru"
+let accountState;
+
+
+accountEmail="gjhjhhhh"
+console.table([accountId, accountEmail,accountPassword,accountState]);
